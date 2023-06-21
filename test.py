2885cbfc55a372afe15a36dcf9b5dec5a0f362b9
Toshimitsu Kimura <lovesyao@gmail.com>
2023-06-21 15:06:06 +0900

    update TorchDeepDanbooru test to support 'torch.compile'

--- test.py
+++ test.py
@@ -22,52 +22,56 @@ with torch.no_grad():# , torch.autocast("cuda")
     # non-jit first run
     y = model(x)[0].detach().cpu().numpy()
 
-    # non-jit measure performance -> 35.49it/s on RTX 3060
+    print("non-jit")
+    # non-jit measure performance -> 79.18it/s on RTX 3060
     for n in tqdm.tqdm(range(10)):
         model(x)
 
     # jit script first run
+    # pip install monkeytype -> error, don't install
     scripted_fn = torch.jit.script(model, example_inputs=[x])
+#    torch._C._jit_pass_inline(scripted_fn.graph) 
     z = scripted_fn(x)[0].detach().cpu().numpy()
 
-    # jit script measure performance -> 4.41it/s on RTX 3060
+    print("script")
+    # jit script measure performance -> 6.88it/s on RTX 3060
     for n in tqdm.tqdm(range(10)):
         scripted_fn(x)
 
     # jit traced first run
     traced_fn = torch.jit.trace(model, example_inputs=[x])
+#    torch._C._jit_pass_inline(traced_fn.graph) 
     aa = traced_fn(x)[0].detach().cpu().numpy()
 
-    # jit traced measure performance -> 42.33it/s on RTX 3060
+    print("traced")
+    # jit traced measure performance -> 94.39it/s on RTX 3060
     for n in tqdm.tqdm(range(10)):
         traced_fn(x)
 
-# $ pip install -U torchdynamo
-# $ pip install -U triton
-#    import torchdynamo
-#    # torchdynamo+triton first run
-#    dynamo_fn = torchdynamo.optimize("inductor")(model)
-#    za = dynamo_fn(x)[0].detach().cpu().numpy()
+    # torchdynamo+triton first run
+    dynamo_fn = torch.compile(model, fullgraph=True)
+    za = dynamo_fn(x)[0].detach().cpu().numpy()
 
-#    # torchdynamo+triton measure performance -> XXit/s on RTX 3060
-#    for n in tqdm.tqdm(range(10)):
-#        dynamo_fn(x)
+    print("compiled - triton")
+    # torchdynamo+triton measure performance -> 105.58it/s on RTX 3060
+    for n in tqdm.tqdm(range(10)):
+        dynamo_fn(x)
 
-#  File "/home/nazo/.local/lib/python3.10/site-packages/torchinductor/triton_ops/autotune.py", line 156, in run
-#    return launcher(
-#  File "<string>", line 4, in launcher
-# TypeError: function takes exactly 13 arguments (10 given)
+    torch._dynamo.reset()
+    # torchdynamo+onnxrt first run
+    # wrong: https://github.com/pytorch/pytorch/blob/e737a8486f2b38c699d6890be295dd6e6d6417ea/docs/source/compile/get-started.rst
+    #        https://github.com/pytorch/pytorch/blob/6b1d6750b98ee1e3ad2fb715194b4a5b247b782d/torch/_dynamo/backends/tensorrt.py
+    # pip install onnxruntime-gpu
+    # right: https://github.com/pytorch/TensorRT/blob/cae6b7c694db1b6a5f287f9fc4e6c40ba08e1079/py/torch_tensorrt/dynamo/backend/backends.py#L26
+    # pip install torch-tensorrt
+    import torch_tensorrt
+    dynamot_fn = torch.compile(model, fullgraph=True, backend="torch_tensorrt") # not "tensorrt"
+    zb = dynamot_fn(x)[0].detach().cpu().numpy()
 
-# $ pip install torch-tensorrt -> not yet available for pytorch 1.3.x
-# https://github.com/pytorch/TensorRT/pull/1477
-#    # torchdynamo+tensorrt first run
-#    dynamo_fn2 = torchdynamo.optimize("fx2trt")(model)
-#    zb = dynamo_fn2(x)[0].detach().cpu().numpy()
-
-#    # torchdynamo+tensorrt measure performance -> XXit/s on RTX 3060
-#    for n in tqdm.tqdm(range(10)):
-#        dynamo_fn2(x)
-
+    print("compiled - tensorrt")
+    # torchdynamo+tensorrt measure performance -> 67.36it/s on RTX 3060
+    for n in tqdm.tqdm(range(10)):
+        dynamot_fn(x)
 
 #    torch.onnx.export(scripted_fn, [x],  "scripted.onnx")
 ##    torch.onnx.export(traced_fn, [x],  "traced.onnx")
@@ -78,7 +82,7 @@ with torch.no_grad():# , torch.autocast("cuda")
 # $ sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/3bf863cc.pub
 # $ sudo add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/ /"
 # $ sudo apt-get update
-# $ sudo apt install cuda-11-7
+# $ sudo apt install cuda-11-8
 #
 # $ python -m pip install colored polygraphy --extra-index-url https://pypi.ngc.nvidia.com
 # $ ~/.local/bin/polygraphy surgeon sanitize scripted.onnx --fold-constants --output scripted_folded.onnx
@@ -91,24 +95,24 @@ with torch.no_grad():# , torch.autocast("cuda")
 # $ export LIBRARY_PATH=$LIBRARY_PATH:/usr/local/cuda/lib64
 # $ pip install pycuda # don't install apt's python3-pycuda
 
-import os
-if not os.path.exists("scripted_folded.trt"):
-    exit()
+#import os
+#if not os.path.exists("scripted_folded.trt"):
+#    exit()
 
-import tensorrt as trt
-import pycuda.autoinit
-import pycuda.driver as cuda
-def load_engine(trt_runtime, engine_path):
-    with open(engine_path, 'rb') as f:
-        engine_data = f.read()
-    engine = trt_runtime.deserialize_cuda_engine(engine_data)
-    return engine
-
-TRT_LOGGER = trt.Logger(trt.Logger.WARNING)
-trt_runtime = trt.Runtime(TRT_LOGGER)
-engine = load_engine(trt_runtime, "scripted_folded.trt")
-stream = cuda.Stream()
-ctx = engine.create_execution_context()
+#import tensorrt as trt
+#import pycuda.autoinit
+#import pycuda.driver as cuda
+#def load_engine(trt_runtime, engine_path):
+#    with open(engine_path, 'rb') as f:
+#        engine_data = f.read()
+#    engine = trt_runtime.deserialize_cuda_engine(engine_data)
+#    return engine
+#
+#TRT_LOGGER = trt.Logger(trt.Logger.WARNING)
+#trt_runtime = trt.Runtime(TRT_LOGGER)
+#engine = load_engine(trt_runtime, "scripted_folded.trt")
+#stream = cuda.Stream()
+#ctx = engine.create_execution_context()
 
 
 def trt_fn(input):
@@ -128,11 +132,12 @@ def trt_fn(input):
     return r
 
 # trt first run
-ab = trt_fn(a)
+#ab = trt_fn(a)
 
 # trt measure performance -> 56.91it/s on RTX 3060
-for n in tqdm.tqdm(range(10)):
-    trt_fn(a)
+#print("trt")
+#for n in tqdm.tqdm(range(10)):
+#    trt_fn(a)
 
 for i, p in enumerate(y):
     if p >= 0.5:
@@ -141,9 +146,9 @@ for i, p in enumerate(y):
         print("z:", model.tags[i], z[i])
     if aa[i] >= 0.5:
         print("aa:", model.tags[i], aa[i])
-#    if za[i] >= 0.5:
-#        print("za:", model.tags[i], za[i])
-#    if zb[i] >= 0.5:
-#        print("zb:", model.tags[i], zb[i])
-    if ab[i] >= 0.5:
-        print("ab:", model.tags[i], ab[i])
+    if za[i] >= 0.5:
+        print("za:", model.tags[i], za[i])
+    if zb[i] >= 0.5:
+        print("zb:", model.tags[i], zb[i])
+#    if ab[i] >= 0.5:
+#        print("ab:", model.tags[i], ab[i])
